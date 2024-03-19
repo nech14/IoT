60f395dedfbf06f9e4ef124eb8570362c53d7802
nech14 <nech14@mail.ru>
2024-03-19 09:24:10 +0800

    API update

---
+++ API_ESP/src/main.py
@@ -0,0 +1,9 @@
+from fastapi import FastAPI
+from src.measurements.router import router as measurements_router
+
+app = FastAPI()
+app.include_router(measurements_router)
+
+@app.get("/")
+def greet():
+    return {"Message":"Hello!!!"}
---
+++ API_ESP/src/measurements/schemas.py
@@ -0,0 +1,9 @@
+# content of measurements/schemas.py
+from datetime import datetime
+from pydantic import BaseModel
+
+class TemperatureValue(BaseModel):
+    value: float 
+    location: str
+    device_id: str
+    timestamp: datetime
