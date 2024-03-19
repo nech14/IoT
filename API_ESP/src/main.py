4c2a74bfc8e69ff3e85d997fc9d22580adbc8787
nech14 <nech14@mail.ru>
2024-03-19 20:30:27 +0800

    update

--- API_ESP/src/main.py
+++ API_ESP/src/main.py
@@ -4,6 +4,7 @@ from src.measurements.router import router as measurements_router
 app = FastAPI()
 app.include_router(measurements_router)
 
+
 @app.get("/")
 def greet():
-    return {"Message":"Hello!!!"}
+    return {"Message": "Hello!!!"}
--- API_ESP/src/measurements/router.py
+++ API_ESP/src/measurements/router.py
@@ -3,19 +3,21 @@ from src.measurements.schemas import TemperatureValue
 
 router = APIRouter()
 
+
 @router.get("/temperature")
 def get_temperature(
-    location: str
+        location: str
 ):
     # TODO select data for location
     return {
-        "values": [1., 2., 3.], # TODO these values are updated
+        "values": [1., 2., 3.],  # TODO these values are updated
         "location": location
     }
 
+
 @router.post("/submit_temperature")
 def submit_temperature(
-    measurement: TemperatureValue
+        measurement: TemperatureValue
 ):
     # TODO save data so we can make get_temperature work properly
     return {"status": "updated"}
